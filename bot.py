d1a4641e89ecb5d50e22a323f5b5b842fa56dfb9
Christian Dähn <christian.daehn@gmail.com>
2017-07-28 19:06:12 +0200

    moved all into functions

--- bot.py
+++ bot.py
@@ -1,72 +1,104 @@
-import sys
+# coding=UTF-8
+
 import logging
-from sheets import SheetConnector
+import sys
+
+# https://github.com/eternnoir/pyTelegramBotAPI
 import telebot
 from cachetools.func import ttl_cache
 
+from sheets import SheetConnector
+
 MUCKI_TRACKER_SHEET_ID = '1x5TECCvP3sF3cmMJiD5frkjkmGOVt2pWgNe2eB2zZtI'
 MUCKI_TRACKER_TEAM_STATUS_RANGE = 'status!A2:B5'
-TOKEN = sys.argv[1]
-
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-log = logging.getLogger(__name__)
-
-log.info('starting %s' % __name__)
-bot = telebot.TeleBot(TOKEN)
+log = None
 
 
-def print_help(message):
-    bot.send_message(message.chat.id, 'available commands:\n'
-                     '/howami - get status of current user\n'
-                     '/howis <name> - get status of specific user\n'
-                     '/howarewe - get status of team\n'
-                     '/team - get members of team'
-                     )
+def _checked_load_logging_config(config_path):
+    from os import path
+    import logging.config
+    expanded_config_path = path.expanduser(config_path)
+    if not path.exists(expanded_config_path):
+        raise Exception(
+            "failed to locate a logging configuration at [%s]. please check the location" % expanded_config_path)
+    logging.config.fileConfig(expanded_config_path)
 
 
-@bot.message_handler(commands=['start', 'help'])
+def startup_bot(arguments):
+    global log
+    _checked_load_logging_config("~/.python/logging.conf")
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+    log = logging.getLogger(__name__)
+    log.info('starting %s' % __name__)
+    try:
+        return telebot.TeleBot(arguments[1])
+    except:
+        log.error('usage: python %s <TOKEN>', arguments[0])
+        sys.exit(255)
+
+
+bot = startup_bot(sys.argv)
+
+
+def main():
+    log.info('started %s. polling...' % __name__)
+    bot.polling()
+
+
+@bot.message_handler(commands=['start', ])
 def start(message):
     bot.reply_to(message, 'Hello %s!' % message.from_user.first_name)
     print_help(message)
 
 
+@bot.message_handler(commands=['start', 'help'])
+def print_help(message):
+    bot.send_message(message.chat.id, 'available commands:\n'
+                                      '/howami - get status of current user\n'
+                                      '/howis <name> - get status of specific user\n'
+                                      '/howarewe - get status of team\n'
+                                      '/team - get members of team'
+                     )
+
+
 @bot.message_handler(commands=['howami'])
 def howami(message):
-    safe_welfare_lookup(message, message.from_user.first_name)
+    _safe_welfare_lookup(message, message.from_user.first_name)
 
 
 @bot.message_handler(commands=['howarewe'])
 def howarewe(message):
-    bot.reply_to(message, '\n'.join([get_welfare_status_for(name) for name in retrieve_team_status().keys()]))
+    bot.reply_to(message, '\n'.join([_get_welfare_status_for(name) for name in _retrieve_team_status().keys()]))
 
 
 @bot.message_handler(commands=['howis'])
 def howis(message):
     try:
         command, name = message.text.split(' ')
-        safe_welfare_lookup(message, name)
+        _safe_welfare_lookup(message, name)
     except ValueError:
         bot.reply_to(message, "can't compile %s. usage: /howis <name>" % message.text)
 
 
 @bot.message_handler(commands=['team'])
 def team(message):
-    bot.reply_to(message, '\n'.join(retrieve_team_status().keys()))
+    bot.reply_to(message, '\n'.join(_retrieve_team_status().keys()))
 
 
-def safe_welfare_lookup(message, name):
+def _safe_welfare_lookup(message, name):
     try:
-        bot.reply_to(message, get_welfare_status_for(name))
+        bot.reply_to(message, _get_welfare_status_for(name))
     except KeyError:
         bot.reply_to(message, 'welfare status for %s not found' % name)
 
 
-def get_welfare_status_for(name):
-    return 'welfare status of %s is %s' % (name, retrieve_team_status()[name])
+def _get_welfare_status_for(name):
+    return 'welfare status of %s is %s' % (name, _retrieve_team_status()[name])
 
 
 @ttl_cache()
-def retrieve_team_status():
+def _retrieve_team_status():
     log.info('loading welfare status')
     team_name_status = SheetConnector(MUCKI_TRACKER_SHEET_ID).values_for_range(MUCKI_TRACKER_TEAM_STATUS_RANGE)
     team_name_status_dict = {}
@@ -76,5 +108,4 @@ def retrieve_team_status():
     return team_name_status_dict
 
 
-log.info('started %s. polling...' % __name__)
-bot.polling()
+main()
