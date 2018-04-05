57089285bdcb5e034beac73ced7bb96516bd116c
Christian Dähn <christian.daehn@gmail.com>
2018-04-05 10:00:08 +0200

    removed deal status

--- cmd.py
+++ cmd.py
@@ -1,5 +1,5 @@
 from my_logging import checked_load_logging_config, basic_logger_config, get_logger
-from sheet import retrieve_team_status, get_welfare_status_for, retrieve_request_status, get_pretty_request_status
+from sheet import retrieve_team_status, get_welfare_status_for
 
 checked_load_logging_config("~/.python/logging.conf")
 
@@ -8,4 +8,3 @@ basic_logger_config()
 log = get_logger(__name__)
 print '\n'.join([get_welfare_status_for(name) for name in retrieve_team_status().keys()])
 
-print get_pretty_request_status()
