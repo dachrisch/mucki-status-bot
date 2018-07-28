7d04ae2d8481e5ea6a9b2a2d3c46996cfb2475bc
Christian Dähn <christian.daehn@gmail.com>
2018-07-28 14:46:50 +0200

    web interface

--- web.py
+++ web.py
@@ -1,9 +1,25 @@
 # coding=UTF-8
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+from concurrent.futures import ThreadPoolExecutor
 from sheet import retrieve_team_status
 
+executor = ThreadPoolExecutor(1)
+
 app = Flask(__name__)
+
+
+class BotLogger(object):
+    def __init__(self):
+        self.__stream = None
+
+    def dump(self):
+        return self.__stream
+
+    def listen(self, stream):
+        self.__stream = stream
+
+
+bot_log = BotLogger()
 
 
 @app.route('/')
@@ -17,9 +33,19 @@ def status():
                            status=retrieve_team_status())
 
 
-@app.route('/telegram')
+@app.route('/telegram', methods=['GET', 'POST'])
 def telegram():
-    return render_template('telegram')
+    if request.method == "POST":
+        token = request.form['token']
+        executor.submit(run_bot, token)
+    return render_template('telegram.html', log=bot_log.dump())
+
+
+def run_bot(token):
+    import subprocess
+
+    p = subprocess.Popen('TELEBOT_TOKEN=%s python2 bot.py' % token)
+    bot_log.listen(p.communicate())
 
 
 if __name__ == '__main__':
