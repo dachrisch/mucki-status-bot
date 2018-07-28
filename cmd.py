4446d2520cba0344131a62e803c239fd3417fc7c
Christian Dähn <christian.daehn@gmail.com>
2018-07-28 11:19:00 +0200

    yaml

--- cmd.py
+++ cmd.py
@@ -1,7 +1,7 @@
 from my_logging import checked_load_logging_config, basic_logger_config, get_logger
 from sheet import retrieve_team_status, get_welfare_status_for
 
-checked_load_logging_config("~/.python/logging.conf")
+checked_load_logging_config("~/.python/logging_debug.conf")
 
 log = get_logger(__name__)
 print '\n'.join([get_welfare_status_for(name) for name in retrieve_team_status().keys()])
