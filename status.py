5848fad213078bf7ba923e276ed2767d51416c21
Christian Dähn <christian.daehn@gmail.com>
2018-08-08 16:51:28 +0200

    shouts from ENV

--- status.py
+++ status.py
@@ -1,6 +1,8 @@
 # coding=UTF-8
 
 from __future__ import division
+
+import os
 from collections import Counter
 
 
@@ -14,21 +16,13 @@ def team_rating_to_shoutout(status_codes):
     team_rating = calculate_team_rating(status_codes)
 
     if team_rating == 1:
-        shout = 'perfect'
-    elif .75 < team_rating < 1:
-        shout = 'awesome'
-    elif .625 < team_rating <= .75:
-        shout = 'yeah'
-    elif .5 < team_rating <= .625:
-        shout = 'well'
-    elif .375 < team_rating <= .5:
-        shout = 'meeehhhh'
-    elif .25 < team_rating <= .375:
-        shout = 'oh oh'
-    elif 0.125 < team_rating <= 0.25:
-        shout = 'damn'
-    elif 0 < team_rating <= 0.125:
-        shout = 'panic'
+        shout = os.getenv('SHOUT_PERFECT', 'unicorn dance')
+    elif .5 < team_rating < 1:
+        shout = os.getenv('SHOUT_AVERAGE', 'well')
+    elif 0 < team_rating <= .5:
+        shout = os.getenv('SHOUT_BAD', 'crying')
+    elif 0 == team_rating:
+        shout = os.getenv('SHOUT_PANIC', 'panic scared')
     else:
         shout = 'freakout'
     return shout
