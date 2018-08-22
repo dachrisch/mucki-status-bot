5b46482e8e6f38ccbfb0884b73668094c9aa3d66
Christian Dähn <christian.daehn@gmail.com>
2018-08-22 13:55:06 +0200

    more E2E tests for highlights

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -11,11 +11,26 @@ class TestHighlights(unittest.TestCase):
     def test_can_execute_howarewe(self):
         TelegramTestBot().assert_can_execute_command(self, 'howarewe')
 
+    def test_can_execute_show_highlights(self):
+        TelegramTestBot().assert_can_execute_command(self, 'show_highlights')
+
+    def test_can_execute_send_highlights(self):
+        TelegramTestBot().assert_can_execute_command(self, 'send_highlights')
+
     def test_can_execute_deals(self):
         TelegramTestBot().assert_can_execute_command(self, 'deals')
 
     def test_start_messages_hello(self):
         TelegramTestBot().assert_command_responses_with(self, 'start', "I'm the bot of the *Südsterne* team.")
+
+    def test_no_highlights(self):
+        TelegramTestBot().assert_command_responses_with(self, 'show_highlights', 'no highlights available')
+
+    def test_one_highlight(self):
+        from telegram_service.bot import highlights
+
+        highlights.add('Chris', '#highlights one')
+        TelegramTestBot().assert_command_responses_with(self, 'show_highlights', 'Chris: one')
 
     def test_deals_messages_all_right(self):
         TelegramTestBot().assert_command_responses_with(self, 'deals', 'Alles rosig!')
