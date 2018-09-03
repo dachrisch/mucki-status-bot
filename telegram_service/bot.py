72f44fc6e24cdbc1afd62810619bf3537d69d913
se-keller <sebastian.sekell@googlemail.com>
2018-09-03 15:53:43 +0200

    removed 1Passowrd links, unreadable/-usable

--- telegram_service/bot.py
+++ telegram_service/bot.py
@@ -96,12 +96,8 @@ def deals(bot, update):
 
 def remote(bot, update):
     _send_and_log(bot, update, 'Google: https://meet.google.com/upv-baht-nyt\n\n'
-                               'Zoom: https://zoom.us/j/6787719716 Login: see 1Password '
-                               'https://it-agile.1password.com/vaults/jra7nmadubtdlor2kotz7wnj44/001'
-                               '/webrqidvxnegpiwuzjacfdrnhe)\n\n '
-                               'Talkyoo: +494095063183 PIN: see 1Password '
-                               'https://it-agile.1password.com/vaults/jra7nmadubtdlor2kotz7wnj44/005'
-                               '/m7s5meb5wzdxhkfsdcfs6bcoka\n\n')
+                               'Zoom: https://zoom.us/j/6787719716 Login: see 1Password\n\n '
+                               'Talkyoo: +494095063183 PIN: see 1Password\n\n')
 
 
 def register_commands(updater):
